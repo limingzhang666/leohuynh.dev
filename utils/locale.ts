baf4932c488569c2e47b4fb9e9034c4ed5ed23f4
zhangliming <zhangliming@dongfangfuli.com>
2025-03-04 23:49:20 +0800

    feat: 解决图片的优化问题

--- utils/locale.ts
+++ utils/locale.ts
@@ -1,7 +1,7 @@
 import { DEFAULT_LOCALE, LOCALES } from '~/constant'
 
 export function getLocale(code: string) {
-  // return LOCALES.find((locale) => locale.code === code)
-  if (code === 'cn') return 'cn' // 适配你的 cn 写法
-  return LOCALES.find((locale) => locale.code === code)?.code || DEFAULT_LOCALE
+  return LOCALES.find((locale) => locale.code === code)
+  // if (code === 'cn') return 'cn' // 适配你的 cn 写法
+  // return LOCALES.find((locale) => locale.code === code)?.code || DEFAULT_LOCALE
 }
